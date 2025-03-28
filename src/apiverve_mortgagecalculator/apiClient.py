28b2e4edc14a9e7f08b5be7d773c05be168de403
APIVerve Team <hello@apiverve.com>
2025-03-28 17:35:58 -0500

    Bug Fixes

--- src/apiverve_mortgagecalculator/apiClient.py
+++ src/apiverve_mortgagecalculator/apiClient.py
@@ -10,10 +10,10 @@ class MortgagecalculatorAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/mortgagecalculator'
+        self.base_url = 'https://api.apiverve.com/v1/mortgagecalculator'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
